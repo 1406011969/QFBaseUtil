11cdbb2833f6f5721457098f2b3021c070ea75d9
风7cheng <cqf_qifeng@163.com>
2020-10-09 10:09:02 +0800

    Release 1.0.0.

---
+++ QFBaseUtilDemo/QFBaseUtilDemo/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QFBaseUtilDemo
+//
+//  Created by cheng on 2020/10/9.
+//  Copyright © 2020 cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
